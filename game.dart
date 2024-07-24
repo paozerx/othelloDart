266f74ac23957820faef4e5529221d2d6c7d5061
paozerx <146060605+paozerx@users.noreply.github.com>
2024-07-24 19:57:30 +0700

    Update game.dart

--- game.dart
+++ game.dart
@@ -1,40 +1,34 @@
 import 'dart:io';
-
-const int size = 8;
-const String empty = '.';
-const String mark = '*';
-const String black = 'X';
-const String white = 'O';
-String current = white;
-String another = black;
-List<List<int>> directions = [
-  [-1, 0],
-  [1, 0],
-  [0, -1],
-  [0, 1],
-  [-1, -1],
-  [-1, 1],
-  [1, -1],
-  [1, 1],
-];
+import 'dart:math';
 
 void main() {
-  Othello game = Othello();
-  game.playGame();
+  Board game = Board();
+  game.playGameOutput();
 }
 
-class Othello {
+class Board {
   late List<List<String>> board;
+  int size = 8;
+  String empty = '.';
+  String mark = '*';
+  String black = '○';
+  String white = '●';
+  String current = '●';
+  String another = '○';
   int coinWhite = 0;
   int coinBlack = 0;
+  List<int> info = [];
+  late Processing processor;
+  Bot bot = Bot();
 
-  Othello() {
+  Board() {
     board = List.generate(size, (_) => List.filled(size, empty));
     board[3][3] = white;
     board[3][4] = black;
     board[4][3] = black;
     board[4][4] = white;
-    checkCoins();
+    processor = Processing(board, size, mark, black, white, empty, this);
+    processor.checkScore();
   }
 
   void printBoard() {
@@ -42,6 +36,106 @@ class Othello {
     for (int i = 0; i < size; i++) {
       print('${i + 1} ${board[i].join(' ')}');
     }
+  }
+
+  void playGameOutput() {
+    while (true) {
+      makerValidMove();
+      printBoard();
+      print('');
+      print('●: $coinWhite     ○: $coinBlack');
+      print('');
+
+      if (processor.isGameOver()) {
+        if (coinWhite > coinBlack) {
+          print('Player ● win');
+        } else if (coinWhite < coinBlack) {
+          print('Player ○ win');
+        } else {
+          print('Draw');
+        }
+        break;
+      }
+      if (current == white) {
+        print('$current\'s turn. Enter row:');
+        int? row = int.tryParse(stdin.readLineSync()!);
+        print('$current\'s turn. Enter column:');
+        int? col = int.tryParse(stdin.readLineSync()!);
+        print('');
+        if (row == null ||
+            col == null ||
+            row <= 0 ||
+            row >= 9 ||
+            col <= 0 ||
+            col >= 9) {
+          print('Invalid input');
+          continue;
+        }
+        if (board[row - 1][col - 1] == mark) {
+          board[row - 1][col - 1] = current;
+          playValid(row - 1, col - 1);
+          processor.removeMark();
+          processor.checkScore();
+          processor.swapTurn();
+        } else {
+          print('Invalid move. Try again.');
+        }
+      } else if (current == black) {
+        info = bot.botPlay(board, mark) as List<int>;
+        print('$current\'s turn. Enter row:');
+        print(info[0] + 1);
+        print('$current\'s turn. Enter column:');
+        print(info[1] + 1);
+        board[info[0]][info[1]] = current;
+        playValid(info[0], info[1]);
+        processor.removeMark();
+        processor.checkScore();
+        processor.swapTurn();
+      }
+    }
+  }
+
+  void makerValidMove() {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (board[i][j] == empty) {
+          if (processor.canPlaceMarker(i, j, current, another)) {
+            board[i][j] = mark;
+          }
+        }
+      }
+    }
+  }
+
+  void playValid(int row, int col) {
+    processor.sendInfoPlay(row, col, current, another);
+  }
+}
+
+class Processing {
+  late List<List<String>> board;
+  int size;
+  String mark;
+  String black;
+  String white;
+  String empty;
+  Board game;
+  final List<List<int>> directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1],
+  ];
+  Processing(this.board, this.size, this.mark, this.black, this.white,
+      this.empty, this.game);
+
+  void swapTurn() {
+    game.current = (game.current == white) ? black : white;
+    game.another = (game.another == black) ? white : black;
   }
 
   void removeMark() {
@@ -54,18 +148,7 @@ class Othello {
     }
   }
 
-  void checkCoins() {
-    coinWhite = 0;
-    coinBlack = 0;
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-        if (board[i][j] == black) coinBlack++;
-        if (board[i][j] == white) coinWhite++;
-      }
-    }
-  }
-
-  bool wincon() {
+  bool isGameOver() {
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < size; j++) {
         if (board[i][j] == mark) return false;
@@ -74,75 +157,27 @@ class Othello {
     return true;
   }
 
-  void playGame() {
-    while (true) {
-      maker();
-      printBoard();
-      print('');
-      print('O: $coinWhite     X: $coinBlack');
-      print('');
-
-      if (wincon()) {
-        if (coinWhite > coinBlack) {
-          print('Player O wins');
-        } else if (coinWhite < coinBlack) {
-          print('Player X wins');
-        } else {
-          print('Draw');
-        }
-        break;
-      }
-
-      print('$current\'s turn. Enter row:');
-      int? row = int.tryParse(stdin.readLineSync()!);
-      print('$current\'s turn. Enter column:');
-      int? col = int.tryParse(stdin.readLineSync()!);
-      print('');
-      if (row == null ||
-          col == null ||
-          row <= 0 ||
-          row >= 9 ||
-          col <= 0 ||
-          col >= 9) {
-        print('Invalid input');
-        continue;
-      }
-
-      if (board[row - 1][col - 1] == mark) {
-        board[row - 1][col - 1] = current;
-        play(row - 1, col - 1);
-        removeMark();
-        checkCoins();
-        current = (current == white) ? black : white;
-        another = (another == black) ? white : black;
-      } else {
-        print('Invalid move. Try again.');
-      }
-    }
-  }
-
-  void maker() {
+  void checkScore() {
+    game.coinWhite = 0;
+    game.coinBlack = 0;
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < size; j++) {
-        if (board[i][j] == empty) {
-          if (canPlaceMarker(i, j)) {
-            board[i][j] = mark;
-          }
-        }
+        if (board[i][j] == black) game.coinBlack++;
+        if (board[i][j] == white) game.coinWhite++;
       }
     }
   }
 
-  bool canPlaceMarker(int row, int col) {
+  bool canPlaceMarker(int row, int col, String current, String another) {
     for (var direction in directions) {
-      if (canFlip(row, col, direction[0], direction[1])) {
+      if (canFlip(row, col, direction[0], direction[1], current, another)) {
         return true;
       }
     }
     return false;
   }
 
-  bool canFlip(int r, int c, int dr, int dc) {
+  bool canFlip(int r, int c, int dr, int dc, String current, String another) {
     int nr = r + dr;
     int nc = c + dc;
     bool hasOpponentBetween = false;
@@ -160,13 +195,13 @@ class Othello {
     return false;
   }
 
-  void play(int row, int col) {
+  void sendInfoPlay(int row, int col, String current, String another) {
     for (var direction in directions) {
-      flipcoins(row, col, direction[0], direction[1]);
+      playFlips(row, col, direction[0], direction[1], current, another);
     }
   }
 
-  void flipcoins(int r, int c, int dr, int dc) {
+  void playFlips(int r, int c, int dr, int dc, String current, String another) {
     List<List<int>> toFlip = [];
     int nr = r + dr;
     int nc = c + dc;
@@ -184,6 +219,24 @@ class Othello {
       }
       nr += dr;
       nc += dc;
+    }
+  }
+}
+
+class Bot {
+  var random = Random();
+  List botPlay(board, mark) {
+    int row;
+    int col;
+    List<int> botplay = [];
+    while (true) {
+      row = random.nextInt(8);
+      col = random.nextInt(8);
+      if (board[row][col] == mark) {
+        botplay.add(row);
+        botplay.add(col);
+        return botplay;
+      }
     }
   }
 }
